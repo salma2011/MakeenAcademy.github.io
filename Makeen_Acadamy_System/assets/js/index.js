909e24c7c1a69f8f98def51584829b036c433c72
Al-Yazid Al-Mukhaini <alyazid88@gmail.com>
2025-05-26 00:21:54 +0400

    Add files via upload

---
+++ Makeen_Acadamy_System/assets/js/index.js
@@ -0,0 +1,66 @@
+const hero = document.querySelector(".hero");
+const images = [
+  "/assets/imgs/slider/s3.jpg",
+  "/assets/imgs/slider/s21.jpg",
+  "/assets/imgs/slider/s1.jpg",
+];
+
+let index = 0;
+
+function changeBackground() {
+  hero.style.backgroundImage = `url('${images[index]}')`;
+  index = (index + 1) % images.length;
+}
+
+// Initial background
+changeBackground();
+
+// Change every 5 seconds
+setInterval(changeBackground, 5000);
+
+const sliders = document.querySelectorAll(".slider");
+const sliderImages = Array.from(sliders).map((slider) =>
+  slider.querySelectorAll("img")
+);
+
+const indices = Array(sliders.length).fill(0); // Current image index for each slider
+let currentSlider = 0; // Start from first slider
+
+function updateSlider(sliderIndex, imgIndex) {
+  const images = sliderImages[sliderIndex];
+  images.forEach((img, i) => {
+    img.classList.toggle("active", i === imgIndex);
+  });
+}
+
+// Initial setup
+sliders.forEach((_, i) => updateSlider(i, 0));
+
+setInterval(() => {
+  // Update current slider
+  indices[currentSlider] =
+    (indices[currentSlider] + 1) % sliderImages[currentSlider].length;
+  updateSlider(currentSlider, indices[currentSlider]);
+
+  // Move to next slider for next tick
+  currentSlider = (currentSlider + 1) % sliders.length;
+}, 2000); // Adjust delay as needed
+
+// Close Button
+
+const form = document.getElementById("registerForm");
+const modal = document.getElementById("thankYouModal");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  modal.style.display = "flex";
+  form.reset();
+});
+
+// Prevent click
+function closeModal() {
+  modal.style.display = "none";
+  window.location.href = "/index.html"; // redirect to courses page
+}
+
+window.closeModal = closeModal;
